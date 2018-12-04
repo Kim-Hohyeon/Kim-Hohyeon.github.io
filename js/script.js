ced2d90d6b1f4bfff84dd9494a068e8c634f5d20
Hohyeon Kim <daumkhh@hanmail.net>
2018-12-04 17:48:50 +0900

    file update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 	$(window).on("load", function(){
-		$(".loading").fadeOut(500, function(){
+		/*$(".loading").fadeOut(500, function(){
 			$(this).remove();
-		});
+		});*/
 	});
 });
