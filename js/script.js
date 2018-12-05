97ae64528a7dc61e95a84fdbbc589b710b40a641
Hohyeon Kim <daumkhh@hanmail.net>
2018-12-05 12:00:48 +0900

    update

--- js/script.js
+++ js/script.js
@@ -9,11 +9,25 @@ $(document).ready(function(){
 		TweenMax.to($("html, body"), 0.8, {scrollTop:0, ease:Power3.easeInOut});
 	});
 
+	$(window).resize(function(){
+		$(".workList").css({"margin-top" : $(".mainVis").height(), "margin-bottom" : $("#footer").height()});
+	});$(window).resize();
+
 	$(window).scroll(function(){
 		if($(window).scrollTop() > 20){
 			$("#header").addClass("scrolled");
 		}else{
 			$("#header").removeClass("scrolled");
+		}
+
+		if($(window).scrollTop() <= $(window).height()){
+			TweenMax.to($('.mainVis'), 0.2, {top:-$(window).scrollTop()/4, ease:Power3.easeOut});
+		}
+
+		if($(window).scrollTop() + $(window).height() >= $(".workList").offset().top + $(".workList").height()){
+			$("#footer").addClass("on");
+		}else{
+			$("#footer").removeClass("on");
 		}
 	});
 });
